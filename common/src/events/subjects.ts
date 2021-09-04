d021bdc17462f548581be8cbcfdfbeb9f4327de9
kurumkan <joomrise@gmail.com>
2021-09-04 21:17:31 +0800

    remove redundant files

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,14 +1,6 @@
 export enum Subjects {
-    TicketCreated = 'ticket:created',
-    TicketUpdated = 'ticket:updated',
-
-    OrderCreated = 'order:created',
-    OrderCancelled = 'order:cancelled',
-
     ExpirationComplete = 'expiration:complete',
     TransformationCreated = 'transformation:created',
-
-    PaymentCreated = 'payment:created',
 
     FileUploaded = 'file:uploaded',
     FileRemoved = 'file:removed'
