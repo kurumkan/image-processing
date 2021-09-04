3ad54e8376806e052bac57235f7eac401899899e
kurumkan <joomrise@gmail.com>
2021-09-04 21:31:24 +0800

    change startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,8 @@ import { app } from './app';
 const PORT = 3000;
 
 const start = async () => {
-  console.log('starting auth')
+  console.log('Starting auth service');
+
   if(!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
